01941237cb9b454f1f58a608ecff1bbd9d7c7261
Kankanis <kankanis.tty@gmail.com>
2025-06-19 10:31:15 +0700

    Add brag route and clean up routes file formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "brag" => "brag#index"
   resources :quests
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+root "quests#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
