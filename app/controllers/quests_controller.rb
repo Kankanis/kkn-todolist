b1bd1a94fbdcb2fe71cd477f727d4c79230639fe
Kankanis <kankanis.tty@gmail.com>
2025-06-19 17:52:48 +0700

    Change quest index order to descending by creation date

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -3,7 +3,7 @@ class QuestsController < ApplicationController
 
   # GET /quests or /quests.json
   def index
-    @quests = Quest.all.order(created_at: :asc)
+    @quests = Quest.all.order(created_at: :desc)
     @new_quest = Quest.new
   end
 
